8bdcf6e8b44c5ec3fc3985bccdc883dfb49a24f3
Fhiz <support@wovencode.net>
2020-01-17 20:58:04 +0100

    - Fix Unity 2019 singleton issue (Domain Reloading disabled)

--- Scripts/Popups/UIPopupNotify.cs
+++ Scripts/Popups/UIPopupNotify.cs
@@ -33,7 +33,7 @@ namespace wovencode
 		// -------------------------------------------------------------------------------
 		protected override void Awake()
 		{
-			if (singleton == null) singleton = this;
+			singleton = this;
 			base.Awake();
 		}
 		
